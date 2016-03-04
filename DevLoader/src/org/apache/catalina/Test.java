1a3175924413b617e78055359bd07aa68e998751
chenb <chenjpu@gmail.com>
2016-03-04 13:30:41 +0800

    代码格式化工具

--- DevLoader/src/org/apache/catalina/Test.java
+++ DevLoader/src/org/apache/catalina/Test.java
@@ -1,25 +1,26 @@
 package org.apache.catalina;
 
 class One {
-	protected int a;
+  protected int a;
 
-	public void setOne(int x) {
-		a = x;
-	}
+  public void setOne(int x) {
+    a = x;
+  }
 
-	public void showOne() {
-		System.out.println("a=" + a);
-	}
+  public void showOne() {
+    System.out.println("a=" + a);
+  }
 }
 
-public class Test extends One {
-	public void setTwo(int x) {
-		setOne(x);
-	}
 
-	public static void main(String[] args) {
-		Test obj = new Test();
-		obj.setTwo(10);
-		System.out.println("a=" + obj.a); // 应改为： obj.showOne();
-	}
+public class Test extends One {
+  public void setTwo(int x) {
+    setOne(x);
+  }
+
+  public static void main(String[] args) {
+    Test obj = new Test();
+    obj.setTwo(10);
+    System.out.println("a=" + obj.a); // 应改为： obj.showOne();
+  }
 }
--- DevLoader/src/org/apache/catalina/loader/DevLoader.java
+++ DevLoader/src/org/apache/catalina/loader/DevLoader.java
@@ -21,193 +21,192 @@ import org.apache.catalina.LifecycleException;
  *
  */
 public class DevLoader extends WebappLoader {
-	//private static final String info = "org.apache.catalina.loader.DevLoader/2.0"; 
-	private static final List<String> IG = new ArrayList<String>();
+  // private static final String info = "org.apache.catalina.loader.DevLoader/2.0";
+  private static final List<String> IG = new ArrayList<String>();
 
-	static {
-		//IG.add("servlet-api");
-		//IG.add("smartas");
-	}
+  static {
+    // IG.add("servlet-api");
+    // IG.add("smartas");
+  }
 
-	private String webClassPathFile = ".#webclasspath";
-	private String projectFile = ".project";
+  private String webClassPathFile = ".#webclasspath";
+  private String projectFile = ".project";
 
-	private static final List<String> EMPTY = Collections.emptyList();
+  private static final List<String> EMPTY = Collections.emptyList();
 
-	public DevLoader() {
-		super();
-	}
+  public DevLoader() {
+    super();
+  }
 
-	public DevLoader(ClassLoader parent) {
-		super(parent);
-	}
+  public DevLoader(ClassLoader parent) {
+    super(parent);
+  }
 
-	/**
-	 * @see org.apache.catalina.Lifecycle#start()
-	 */
-	public void startInternal() throws LifecycleException {
-		log("Starting DevLoader");
-		//setLoaderClass(DevWebappClassLoader.class.getName());
+  /**
+   * @see org.apache.catalina.Lifecycle#start()
+   */
+  public void startInternal() throws LifecycleException {
+    log("Starting DevLoader");
+    // setLoaderClass(DevWebappClassLoader.class.getName());
 
-		super.startInternal();
+    super.startInternal();
 
-		ClassLoader cl = super.getClassLoader();
-		if (cl instanceof WebappClassLoader == false) {
-			logError("Unable to install WebappClassLoader !");
-			return;
-		}
+    ClassLoader cl = super.getClassLoader();
+    if (cl instanceof WebappClassLoader == false) {
+      logError("Unable to install WebappClassLoader !");
+      return;
+    }
 
-		File prjDir = getProjectRootDir().getParentFile();
+    File prjDir = getProjectRootDir().getParentFile();
 
-		if (prjDir == null) {
-			logError("[" + projectFile + "] file not found");
-			return;
-		}
-		WebappClassLoader devCl = (WebappClassLoader) cl;
+    if (prjDir == null) {
+      logError("[" + projectFile + "] file not found");
+      return;
+    }
+    WebappClassLoader devCl = (WebappClassLoader) cl;
 
-		List<String> webClassPathEntries = readWebClassPathEntries();
-		//StringBuffer classpath   = new StringBuffer();
-		for (String entry : webClassPathEntries) {
-			//����jar����
-			if (entry.endsWith("*.jar")) {
-				loadJar(devCl, prjDir, entry.substring(0, entry.length() - 5));
-			} else {
-				loadPath(devCl, prjDir, entry);
-			}
-		}
-	}
+    List<String> webClassPathEntries = readWebClassPathEntries();
+    // StringBuffer classpath = new StringBuffer();
+    for (String entry : webClassPathEntries) {
+      // ����jar����
+      if (entry.endsWith("*.jar")) {
+        loadJar(devCl, prjDir, entry.substring(0, entry.length() - 5));
+      } else {
+        loadPath(devCl, prjDir, entry);
+      }
+    }
+  }
 
-	private void loadPath(WebappClassLoader devCl, File prjDir, String entry) {
-		File f = new File(prjDir, entry);
-		//����·��
-		if (!f.exists()) {
-			f = new File(entry);
-			if (!f.exists()) {
-				logError(entry + " does not exist !");
-			}
-			return;
-		}
+  private void loadPath(WebappClassLoader devCl, File prjDir, String entry) {
+    File f = new File(prjDir, entry);
+    // ����·��
+    if (!f.exists()) {
+      f = new File(entry);
+      if (!f.exists()) {
+        logError(entry + " does not exist !");
+      }
+      return;
+    }
 
-		if (f.isDirectory() && entry.endsWith("/") == false)
-			f = new File(f, "/");
-		try {
-			URL url = f.toURI().toURL();
-			devCl.addRepository(url.toString());
-			log("added " + url.toString());
-		} catch (MalformedURLException e) {
-			logError(entry + " invalid (MalformedURL)");
-		}
-	}
+    if (f.isDirectory() && entry.endsWith("/") == false) f = new File(f, "/");
+    try {
+      URL url = f.toURI().toURL();
+      devCl.addRepository(url.toString());
+      log("added " + url.toString());
+    } catch (MalformedURLException e) {
+      logError(entry + " invalid (MalformedURL)");
+    }
+  }
 
-	private boolean ignore(String name) {
-		for (String jar : IG) {
-			if (name.contains(jar)) {
-				return false;
-			}
-		}
-		return true;
-	}
+  private boolean ignore(String name) {
+    for (String jar : IG) {
+      if (name.contains(jar)) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-	private void loadJar(WebappClassLoader devCl, File prjDir, String entry) {
-		//entry = entry.substring(0,entry.length() - 5);
-		File f = new File(prjDir, entry);
-		//����·��
-		if (!f.exists()) {
-			f = new File(entry);
-			if (!f.exists()) {
-				logError(entry + " does not exist !");
-				return;
-			}
-		}
+  private void loadJar(WebappClassLoader devCl, File prjDir, String entry) {
+    // entry = entry.substring(0,entry.length() - 5);
+    File f = new File(prjDir, entry);
+    // ����·��
+    if (!f.exists()) {
+      f = new File(entry);
+      if (!f.exists()) {
+        logError(entry + " does not exist !");
+        return;
+      }
+    }
 
-		FileFilter filter = new FileFilter() {
-			public boolean accept(File file) {
-				return ignore(file.getName()) && file.getName().endsWith(".jar");
-			}
-		};
+    FileFilter filter = new FileFilter() {
+      public boolean accept(File file) {
+        return ignore(file.getName()) && file.getName().endsWith(".jar");
+      }
+    };
 
-		for (File file : f.listFiles(filter)) {
-			try {
-				URL url = file.toURI().toURL();
-				devCl.addRepository(url.toString());
-				log("added " + url.toString());
-			} catch (MalformedURLException e) {
-				logError(file + " invalid (MalformedURL)");
-			}
-		}
-	}
+    for (File file : f.listFiles(filter)) {
+      try {
+        URL url = file.toURI().toURL();
+        devCl.addRepository(url.toString());
+        log("added " + url.toString());
+      } catch (MalformedURLException e) {
+        logError(file + " invalid (MalformedURL)");
+      }
+    }
+  }
 
-	protected void log(String msg) {
-		System.out.println("[DevLoader] " + msg);
-	}
+  protected void log(String msg) {
+    System.out.println("[DevLoader] " + msg);
+  }
 
-	protected void logError(String msg) {
-		System.err.println("[DevLoader] Error: " + msg);
-	}
+  protected void logError(String msg) {
+    System.err.println("[DevLoader] Error: " + msg);
+  }
 
-	protected List<String> readWebClassPathEntries() {
+  protected List<String> readWebClassPathEntries() {
 
-		File webappDir = getWebappDir();
-		log("projectdir=" + webappDir.getAbsolutePath());
+    File webappDir = getWebappDir();
+    log("projectdir=" + webappDir.getAbsolutePath());
 
-		return loadWebClassPathFile(webappDir);
-	}
+    return loadWebClassPathFile(webappDir);
+  }
 
-	protected File getProjectRootDir() {
-		File rootDir = getWebappDir();
+  protected File getProjectRootDir() {
+    File rootDir = getWebappDir();
 
-		FileFilter filter = new FileFilter() {
-			public boolean accept(File file) {
-				return (file.getName().equalsIgnoreCase(projectFile));
-			}
-		};
-		while (rootDir != null) {
-			File[] files = rootDir.listFiles(filter);
-			if (files != null && files.length >= 1) {
-				return files[0].getParentFile();
-			}
-			rootDir = rootDir.getParentFile();
-		}
-		return null;
-	}
+    FileFilter filter = new FileFilter() {
+      public boolean accept(File file) {
+        return (file.getName().equalsIgnoreCase(projectFile));
+      }
+    };
+    while (rootDir != null) {
+      File[] files = rootDir.listFiles(filter);
+      if (files != null && files.length >= 1) {
+        return files[0].getParentFile();
+      }
+      rootDir = rootDir.getParentFile();
+    }
+    return null;
+  }
 
-	protected List<String> loadWebClassPathFile(File prjDir) {
-		File cpFile = new File(prjDir, webClassPathFile);
-		if (!cpFile.exists()) {
-			return EMPTY;
-		}
-		LineNumberReader lr = null;
-		try {
-			lr = new LineNumberReader(new FileReader(cpFile));
-			List<String> rc = new ArrayList<String>();
-			String line = null;
-			while ((line = lr.readLine()) != null) {
-				line = line.trim();
-				if (line.length() > 0 && !line.startsWith("#")) {
-					line = line.replace('\\', '/');
-					rc.add(line);
-				}
-			}
-			return rc;
-		} catch (IOException ioEx) {
-			return EMPTY;
-		} finally {
-			if (lr != null) {
-				try {
-					lr.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+  protected List<String> loadWebClassPathFile(File prjDir) {
+    File cpFile = new File(prjDir, webClassPathFile);
+    if (!cpFile.exists()) {
+      return EMPTY;
+    }
+    LineNumberReader lr = null;
+    try {
+      lr = new LineNumberReader(new FileReader(cpFile));
+      List<String> rc = new ArrayList<String>();
+      String line = null;
+      while ((line = lr.readLine()) != null) {
+        line = line.trim();
+        if (line.length() > 0 && !line.startsWith("#")) {
+          line = line.replace('\\', '/');
+          rc.add(line);
+        }
+      }
+      return rc;
+    } catch (IOException ioEx) {
+      return EMPTY;
+    } finally {
+      if (lr != null) {
+        try {
+          lr.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
 
-	protected ServletContext getServletContext() {
-		return ((Context) getContainer()).getServletContext();
-	}
+  protected ServletContext getServletContext() {
+    return ((Context) getContainer()).getServletContext();
+  }
 
-	protected File getWebappDir() {
-		File webAppDir = new File(getServletContext().getRealPath("/"));
-		return webAppDir;
-	}
+  protected File getWebappDir() {
+    File webAppDir = new File(getServletContext().getRealPath("/"));
+    return webAppDir;
+  }
 }
