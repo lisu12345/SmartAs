21f8fbdcf4238d56f9a84611bc51246300238856
chenb <chenjpu@gmail.com>
2016-03-24 14:53:22 +0800

    业务异常处理

--- smartas-core/src/main/java/com/fiberhome/smartas/core/ui/ExceptionHandlerUI.java
+++ smartas-core/src/main/java/com/fiberhome/smartas/core/ui/ExceptionHandlerUI.java
@@ -11,11 +11,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.support.WebContentGenerator;
 
+import com.fiberhome.smartas.core.BusinessAccessException;
 import com.fiberhome.smartas.core.model.ErrorVO;
 
 /**
@@ -64,5 +66,23 @@ public abstract class ExceptionHandlerUI extends WebContentGenerator {
   @ResponseStatus(code = HttpStatus.BAD_REQUEST)
   public ErrorVO exception(Exception e) {
     return sys(e);
+  }
+
+  /**
+   * 
+   * @return
+   */
+  @ExceptionHandler({Exception.class})
+  @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+  public ErrorVO exception(BusinessAccessException e) {
+    String message;
+    try {
+      message = messageSource.getMessage(e.getErrorCode(), e.getArguments(), null);
+    } catch (NoSuchMessageException e1) {
+      logger.error(e1.getMessage(), e1);
+      message = e.getErrorCode();
+    }
+    logger.error(e.getMessage(), e);
+    return new ErrorVO(e.getErrorCode(), message, getStackTrace(e));
   }
 }
